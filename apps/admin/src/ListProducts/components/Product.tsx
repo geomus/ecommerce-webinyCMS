0cdafae32f0fc465a21660b628b93c2375baee7b
mativiscusso <mativiscusso@gmail.com>
2020-09-22 14:51:04 -0300

    https://trello.com/c/cW3Jv1jR/

--- apps/admin/src/ListProducts/components/Product.tsx
+++ apps/admin/src/ListProducts/components/Product.tsx
@@ -1,19 +1,54 @@
-import React from 'react'
-import { Image } from "@webiny/ui/Image";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import { ReactComponent as ShopCart } from "@icons/material/svg/cart-plus.svg";
 
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
 
-export default function Product({id, images, name, price}) {
+export default function Product({ id, images, name, price }) {
+    const classes = useStyles();
+
     return (
-        <div className="product" key={id}>
-        <Image className="img" alt="product img" src={images} />
-        <h2>{name}</h2>
-        <h3 style={{ color: "#D51E42" }}>$ {price}</h3>
-        <form action="/wonder-slug/product-detail" method="get">
-            <input type="hidden" name="id" value={id}/>
-            <input type="submit" value="Ver mas"/>
-        </form>
-        <button className="btn" type="button"><ShopCart /></button>
-     </div>
-    )
+        <Card className={classes.root} key={id}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={images}
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {name}
+                    </Typography>
+                    <Typography variant="body1" color="textSecondary" component="p">
+                        {price}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <form action="/wonder-slug/product-detail" method="get">
+                    <input type="hidden" name="id" value={id} />
+                    <Button size="small" color="primary" type="submit">
+                        Ver Mas
+                </Button>
+                </form>
+                <Button size="small" color="primary">
+                    <ShopCart />
+                </Button>
+            </CardActions>
+        </Card>
+    );
 }
