742497571660f35a82c0b739ae40058c7a090a21
mativiscusso <mativiscusso@gmail.com>
2020-09-28 08:34:02 -0300

    add component form checkout in site folder

--- apps/site/src/App.tsx
+++ apps/site/src/App.tsx
@@ -5,9 +5,10 @@ import ListProducts from './ListProducts';
 import ProductSearch from './ProductSearch';
 import ButtonCartHome from './ButtonCartHome';
 import Cart from './Cart';
+import FormCheckout from './FormCheckout';
 
 export default (params: SiteAppOptions = {}) => {
     const plugins = params.plugins || [];
-    plugins.push(ProductDetail(), ProductSearch(), ListProducts(), ButtonCartHome(), Cart());
+    plugins.push(ProductDetail(), ProductSearch(), ListProducts(), ButtonCartHome(), Cart(), FormCheckout());
     return createSite({ ...params, plugins });
 };
