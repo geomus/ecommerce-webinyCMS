fe07959dfcb54b8f5a76f901723d1347f7444278
mativiscusso <mativiscusso@gmail.com>
2020-11-04 13:50:23 -0300

    Restyling menu admin

--- apps/admin/src/Dashboard/Menus/DashboardMenu.tsx
+++ apps/admin/src/Dashboard/Menus/DashboardMenu.tsx
@@ -1,24 +1,42 @@
 import React from "react";
-import StorefrontIcon from '@material-ui/icons/Storefront';
+// import StorefrontIcon from '@material-ui/icons/Storefront';
+import AllInboxIcon from '@material-ui/icons/AllInbox';
+import PeopleIcon from '@material-ui/icons/People';
+import ListAltIcon from '@material-ui/icons/ListAlt';
 import { AdminMenuPlugin } from "@webiny/app-admin/types"
 
 export default  [{
     type: "admin-menu",
-    name: "admin-menu-dashboard",
-    render({ Menu, Section, Item }) {
+    name: "admin-menu-products",
+    render({ Menu, Item }) {
       return (
-        <Menu name="shop" icon={<StorefrontIcon />} label="Dashboard">
-          <Section label={"Products Manager"}>
-            <Item label={"Products"} path="/products" />
+        <Menu name="products" icon={<AllInboxIcon />} label="Products">
+            <Item label={"Products List"} path="/products" />
             <Item label={"Prices"} path="/prices" />
-          </Section>
-          <Section label={"Users Manager"}>
-            <Item label={"Users"} path="/users" />
-          </Section>
-          <Section label={"Stats"}>
-            <Item label={"Orders"} path="/orders" />
-          </Section>
         </Menu>
       );
     }
-  } as AdminMenuPlugin]
+  } as AdminMenuPlugin,
+  {
+    type: "admin-menu",
+    name: "admin-menu-users",
+    render({ Menu, Item }) {
+      return (
+        <Menu name="users" icon={<PeopleIcon />} label="Users">
+            <Item label={"Users List"} path="/users" />
+        </Menu>
+      );
+    }
+  } as AdminMenuPlugin,
+  {
+    type: "admin-menu",
+    name: "admin-menu-orders",
+    render({ Menu, Item }) {
+      return (
+        <Menu name="orders" icon={<ListAltIcon />} label="Orders">
+            <Item label={"Orders"} path="/orders" />
+        </Menu>
+      );
+    }
+  } as AdminMenuPlugin,
+]
