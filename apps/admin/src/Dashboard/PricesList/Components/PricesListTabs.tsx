bac14ae2812aa37e676258430d0fc3b6240260b4
mativiscusso <mativiscusso@gmail.com>
2021-01-11 09:04:16 -0300

    https://trello.com/c/2YwDohq6/

--- apps/admin/src/Dashboard/PricesList/Components/PricesListTabs.tsx
+++ apps/admin/src/Dashboard/PricesList/Components/PricesListTabs.tsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import { listPricesList, products } from "../../../graphql/query";
+import { listPricesList, products, updatePriceListIsDefault } from "../../../graphql/query";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import PricesListTable from "./PricesListTable";
 import PricesCategoryBtnCreate from "./PricesListBtnCreate";
 import PricesListDelete from './PricesListDelete'
@@ -18,6 +18,13 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import PriceListBtnEdit from './PricesListBtnEdit'
+import Radio from "@material-ui/core/Radio";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from "@material-ui/core/Button";
 
 
 
@@ -69,9 +76,24 @@ const useStyles = makeStyles((theme) => ({
 
 export default function PricesTabsListPrices() {
     const classes = useStyles();
-
+    const [selectedValue, setSelectedValue] = React.useState({});
+    const [radioValue, setRadioValue] = React.useState('');
+    const [open, setOpen] = React.useState(false);
     const { loading, error, data } = useQuery(listPricesList);
     const { loading: productsLoading, error: productsError, data: productsData } = useQuery(products);
+    const [updateStatusPriceList] = useMutation(updatePriceListIsDefault, {
+        refetchQueries: () => [{ query: listPricesList }]
+    })
+
+    React.useEffect(() => {
+        if (!loading && data) {
+            data.pricesList.listPricesList.data.forEach(list => {
+                if (list.isDefaultOnSite) {
+                    setSelectedValue(list.id)
+                }
+            });
+        }
+    }, [loading, data]);
 
     if (loading || productsLoading) {
         return (
@@ -87,7 +109,27 @@ export default function PricesTabsListPrices() {
         return <h1> error </h1>;
     }
 
+    const handleChange = (event) => {
+        setOpen(true);
+        setRadioValue(event.target.value);
+    };
 
+    const handleSubmitIsDefault = async () => {
+        setSelectedValue(radioValue)
+        setOpen(false);
+        await data.pricesList.listPricesList.data.forEach(async list => {
+            if (list.id == radioValue) {
+                await updateStatusPriceList({ variables: { data: { isDefaultOnSite: true }, id: radioValue } })
+            } else {
+                await updateStatusPriceList({ variables: { data: { isDefaultOnSite: false }, id: list.id } })
+            }
+        });
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+        setRadioValue('')
+    };
 
     return (
         <div className={classes.root}>
@@ -108,6 +150,7 @@ export default function PricesTabsListPrices() {
                                 <TableCell align="center">Porcentaje</TableCell>
                                 <TableCell align="right"></TableCell>
                                 <TableCell align="right"></TableCell>
+                                <TableCell align="center">Default</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -117,20 +160,50 @@ export default function PricesTabsListPrices() {
                                         {row.name}
                                     </TableCell>
                                     <TableCell align="center">
-                                            {row.percent}%
+                                        {row.percent}%
                                     </TableCell>
                                     <TableCell align="right">
-                                        <PriceListBtnEdit priceList={row}/>
+                                        <PriceListBtnEdit priceList={row} />
                                     </TableCell>
                                     <TableCell align="right">
                                         <PricesListDelete row={row} />
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <Radio
+                                            checked={selectedValue === row.id}
+                                            onChange={handleChange}
+                                            value={row.id}
+                                            name="isDefaultOnSite"
+                                            inputProps={{ 'aria-label': 'Is default on site' }}
+                                        />
                                     </TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
-                <PricesListTable products={productsData.products.listProducts.data}/>
+                <PricesListTable products={productsData.products.listProducts.data} />
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="is-default-on-site"
+                    aria-describedby="is-default-on-site"
+                >
+                    <DialogTitle id="is-default-on-site">¿Esta seguro que quiere configurar en el sitio esta lista por defecto?</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="is-default-on-site">
+                            Recuerde que al aceptar la configuracion, se modificaran todos los precios que se mostraran en el sitio.
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="primary">
+                            Cancelar
+                        </Button>
+                        <Button onClick={handleSubmitIsDefault} color="primary" autoFocus>
+                            Aceptar
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </Container>
         </div>
     );
